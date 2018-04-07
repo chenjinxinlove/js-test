409a2193400bf2ef10ce276c493a201adc36e1b0
chenjinxinlove <505150704@qq.com>
2018-04-07 22:14:30 +0800

    - 91

--- 30SecondsOfCode/80.json.js
+++ 30SecondsOfCode/80.json.js
@@ -45,5 +45,94 @@
     "es6": "const merge = (...objs) =>[...objs].reduce((acc, obj) =>Object.keys(obj).reduce((a, k) => {acc[k] = acc.hasOwnProperty(k) ? [].concat(acc[k]).concat(obj[k]) : obj[k];return acc;}, {}),{});",
     "test": "",
     "difficulty":"2"
-  }
+  },
+  {
+    "id": "nest",
+    "num": "84",
+    "name": "",
+    "descEn":"Given a flat array of objects linked to one another, it will nest them recursively. Useful for nesting comments, such as the ones on reddit.com.",
+    "descCn":"给定一个相互关联的平面数组，它会递归地嵌套它们。用于嵌套注释，如reddit.com上的注释。",
+    "es5": "",
+    "es6": "const nest = (items, id = null, link = 'parent_id') =>items.filter(item => item[link] === id).map(item => ({ ...item, children: nest(items, item.id) }));",
+    "test": "",
+    "difficulty":"3"
+  },
+  {
+    "id": "objectFromPairs",
+    "num": "85",
+    "name": "",
+    "descEn":"Creates an object from the given key-value pairs.",
+    "descCn":"从给定的键值对创建一个对象。",
+    "es5": "",
+    "es6": "const objectFromPairs = arr => arr.reduce((a, v) => ((a[v[0]] = v[1]), a), {});",
+    "test": "objectFromPairs([['a', 1], ['b', 2]]); // {a: 1, b: 2}",
+    "difficulty":"2"
+  },
+  {
+    "id": "omit",
+    "num": "86",
+    "name": "",
+    "descEn":"Omits the key-value pairs corresponding to the given keys from an object.",
+    "descCn":"从一个对象中，省略与给定键对应的键值对。",
+    "es5": "",
+    "es6": "const omit = (obj, arr) =>Object.keys(obj).filter(k => !arr.includes(k)).reduce((acc, key) => ((acc[key] = obj[key]), acc), {});",
+    "test": "omit({ a: 1, b: '2', c: 3 }, ['b']); // { 'a': 1, 'c': 3 }",
+    "difficulty":"2"
+  },
+  {
+    "id": "omitby",
+    "num": "87",
+    "name": "",
+    "descEn":"Omits the key-value pairs corresponding to the given keys from an object.",
+    "descCn":"从一个对象中，省略与给定键对应的键值对。",
+    "es5": "",
+    "es6": "const omitBy = (obj, fn) =>Object.keys(obj).filter(k => !fn(obj[k], k)).reduce((acc, key) => ((acc[key] = obj[key]), acc), {});",
+    "test": "omitBy({ a: 1, b: '2', c: 3 }, x => typeof x === 'number'); // { b: '2' }",
+    "difficulty":"2"
+  },
+  {
+    "id": "pick",
+    "num": "88",
+    "name": "",
+    "descEn":"Picks the key-value pairs corresponding to the given keys from an object.",
+    "descCn":"从对象中选择对应于给定键的键值对。",
+    "es5": "",
+    "es6": "const pick = (obj, arr) =>arr.reduce((acc, curr) => (curr in obj && (acc[curr] = obj[curr]), acc), {});",
+    "test": "pick({ a: 1, b: '2', c: 3 }, ['a', 'c']); // { 'a': 1, 'c': 3 }",
+    "difficulty":"2"
+  },
+  {
+    "id": "shallowClone",
+    "num": "89",
+    "name": "",
+    "descEn":"Creates a shallow clone of an object.",
+    "descCn":"创建对象的一个浅克隆。",
+    "es5": "",
+    "es6": "const shallowClone = obj => Object.assign({}, obj);",
+    "test": "",
+    "difficulty":"1"
+  },
+  {
+    "id": "size",
+    "num": "90",
+    "name": "",
+    "descEn":"Get size of arrays, objects or strings.",
+    "descCn":"获取数组、对象或字符串的大小。",
+    "es5": "",
+    "es6": "const size = val =>Array.isArray(val) ? val.length: val && typeof val === 'object' ? val.size || val.length || Object.keys(val).length: typeof val === 'string' ? new Blob([val]).size : 0;",
+    "test": "size([1, 2, 3, 4, 5]); // 5size('size'); // 4size({ one: 1, two: 2, three: 3 }); // 3",
+    "difficulty":"2"
+  },
+  {
+    "id": "shallowClone",
+    "num": "91",
+    "name": "",
+    "descEn":"Unlatten an object with the paths for keys.",
+    "descCn":"创建对象的一个浅克隆。",
+    "es5": "",
+    "es6": "",
+    "test": "unflattenObject({ 'a.b.c': 1, d: 1 }); // { a: { b: { c: 1 } }, d: 1 }",
+    "difficulty":"4"
+  },
+
 ]
