e41dbc1bd62e657b8acb6d4292aa83e6f127b512
陈金鑫 <505150704@qq.com>
2018-06-25 12:18:37 +0800

    40-18

--- 30SecondsOfCode/index.40.1.js
+++ 30SecondsOfCode/index.40.1.js
@@ -1,4 +1,5 @@
 const pipeAsyncFunctions = (...fns) => arg => fns.reduce((p, f) => p.then(f), Promise.resolve(arg));
+const pipeAsyncFunctions = (...fns) => arg => fns.reduce((p, f) => p.then(f), Promise.resolve(arg));
 
 const sum = pipeAsyncFunctions(
     x => x + 1,
@@ -16,22 +17,41 @@ const sum = pipeAsyncFunctions(
       func(...args, (err, result) => (err ? reject(err): resolve(result)))
   )
 
+  const promisify = func => (...args) =>
+    new Promise((resolve, reject) => 
+    func(...args, (err, result) => (err ? reject(err): resolve(result)))
+  )
+
+  const deepFlatten = arr => [].concat(...arr.map((v) => Array.isArray(v) ? deepFlatten(v) : v))
   const deepFlatten = arr => [].concat(...arr.map((v) => Array.isArray(v) ? deepFlatten(v) : v))
 
-
-  const differenceBy = ((a, b , fn) => {
+  const differenceBy = ((a, b, fn) => {
     const s = new Set(...fn(b));
     return b.filter(v => !s.has(fn(v)))
   })
+
+  const differenceBy = ((a, b, fn) => {
+    const s = new Set(...fn(b));
+    return b.filter(v => !s.has((fn(v))))
+  })
+
+
+  const filterNonUnique = (arr) => arr.filter(v => v.indexOf(arr) === v.lastIndexOf(arr))
 
   const filterNonUnique = (arr) => arr.filter(v => v.indexOf(arr) === v.lastIndexOf(arr))
 
   const flatten = (arr, depth = 1) => 
     arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1): v), [])
 
+  const flatten = (arr, depth = 1) =>
+    arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1): v), [])  
 
 const initalize2DArray = (w, h, val = null) => 
   Array.from({ length: h }).map(() => Array.from({ length: w }).fill(val));
+
+
+const initalize2DArray = (w, h, val = null) =>
+  Array.from({length: h}).map(() => Array.from({length: w}).fill(val))  
 
 const shuffle = ([...arr]) => {
   let m = arr.length;
@@ -42,6 +62,15 @@ const shuffle = ([...arr]) => {
   return arr;
 }
 
+
+const shuffle = ([...arr]) => {
+  let m = arr.length;
+  while (m) {
+    const i = Math.floor(Math.random() * m--);
+    [arr[m], arr[i]] = [arr[i], arr[m]];
+  }
+  return arr;
+}
 const  union = (a, b) => Array.from(new Set([...a, ...b]))
 
 // const union = (a, b) => [...new Set([...a, ...b])]
@@ -50,11 +79,20 @@ const  union = (a, b) => Array.from(new Set([...a, ...b]))
 const pick = (obj, arr) => 
   arr.reduce((acc, curr) => (curr in obj && (acc[curr] = obj[curr]), acc), {});
 
+const pick = (obj, arr) =>
+  arr.reduce((acc, curr) => (curr in obj && (acc[curr] = obj[curr]), acc), {});
 
 const zip = (...arrays) => {
   const maxLength = Math.max(...arrays.map(x => x.length));
   return Array.from({length: maxLength}).map((_, i) => {
     return Array.from({ length: arrays.length }, (_, k) => arrays[k][i])
+  })
+}
+
+const zip = (...arrays) => {
+  const maxLength = Math.max(...arrays.map(x => x.length));
+  return Array.from({length: maxLength}).map((_, i) => {
+    return Array.from({length: arrays.length}, (_, k) => arrays[k][i])
   })
 }
 
@@ -66,10 +104,46 @@ const unzip = arr =>
     }).map(x => [])
   )
 
+const unzip = arr => 
+  arr.reduce(
+    (acc,val) => (val.forEach((v, i) => acc[i].push(v)), acc),
+    Array.from({
+      length: Math.max(...arr.map(x => x.length))
+    }).map(x => [])
+  )
+
 const chainAsync = fns => {
   let curr = 0;
   const next = () => fns[curr++](next);
   next();
-}  
+} 
+
+const chainAsync = fns => {
+  let curr = 0;
+  const next = () => fns[curr++](next);
+  next();
+}
 
 const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+
+
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+
+const curry = (fn, arity = fn.length, ...args) =>
+  arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args);
+
+
+const delay = d => new Promise(r => setTimeout(r, d));
+runPromisesInSeries([() => delay(1000), () => delay(2000)]);
+
+
+const runPromisesInSeries = ps => ps.reduce((p, next) => p.then(next), Promise.resolve())
+
+
+const debounce = (fn, ms = 0) => {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms)
+  }
+}
