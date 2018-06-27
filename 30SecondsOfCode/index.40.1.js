de76488186722a5f4d5ca8ecba219387d465d079
陈金鑫 <505150704@qq.com>
2018-06-27 11:19:55 +0800

    40 18

--- 30SecondsOfCode/index.40.1.js
+++ 30SecondsOfCode/index.40.1.js
@@ -147,3 +147,50 @@ const debounce = (fn, ms = 0) => {
     timeoutId = setTimeout(() => fn.apply(this, args), ms)
   }
 }
+
+const memoize = fn => {
+  const cache = new Map();
+  const cached = function(val) {
+    return cache.has(val) ? cache.get(val) : cache.set(val, fn.call(this, val) && cache.get(val));
+  };
+  cached.cache = cache;
+  return cached;
+}
+
+const once = fn => {
+  let called = false;
+  return function(...args) {
+    if (called) return;
+    called = true;
+    return fn.apply(this, args);
+  }
+}
+
+var throttle = function ( fn, interval ) {
+  var __self = fn, // 保存需要被延迟执行的函数引用
+          timer, // 定时器
+          firstTime = true; // 是否是第一次调用
+  return function () {
+      var args = arguments,
+              __me = this;
+      if ( firstTime ) { // 如果是第一次调用，不需延迟执行
+          __self.apply(__me, args);
+          return firstTime = false;
+      }
+      if ( timer ) { // 如果定时器还在，说明前一次延迟执行还没有完成
+          return false;
+      }
+      timer = setTimeout(function () { // 延迟一段时间执行
+          clearTimeout(timer);
+          timer = null;
+          __self.apply(__me, args);
+      }, interval || 5000 );
+  };
+};
+
+
+const uncurry = (fn, n = 1) => (...args) => {
+  const next = acc => args => args.reduce((x, y) => x(y), acc);
+  if (n > args.length) throw new RangeError('arguments too few');
+  return next(fn)(args.slice(0, n))
+}
