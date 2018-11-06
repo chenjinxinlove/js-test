ffeb60c65690ac7846430b39091c6c7ea93da5e7
chenjinxinlove <505150704@qq.com>
2018-11-06 22:27:11 +0800

    connet

--- rxjs/index.js
+++ rxjs/index.js
@@ -65,16 +65,45 @@
 
 // console.info('tag', findIndex$)
 
-const source$ = Rx.Observable.range(1, 100).takeWhile(
-    value => value % 2 === 0
-).subscribe(
-    v => {
-        console.log('Value', v)
-    },
-    e => {
-        console.log('Error', e)
-    },
-    () => {
-        console.log('Completed')
-    }
-);
+// const source$ = Rx.Observable.range(1, 100).takeWhile(
+//     value => value % 2 === 0
+// ).subscribe(
+//     v => {
+//         console.log('Value', v)
+//     },
+//     e => {
+//         console.log('Error', e)
+//     },
+//     () => {
+//         console.log('Completed')
+//     }
+// );
+
+var source = Rx.Observable.interval(500)
+var subject = new Rx.Subject()
+var multicasted = source.multicast(subject)
+var subscription1, subscription2, subscriptionConnect
+
+subscription1 = multicasted.subscribe({
+	next: v => console.log('observerA: ' + v),
+})
+// 这里我们应该调用 `connect()`，因为 `multicasted` 的第一个
+// 订阅者关心消费值
+subscriptionConnect = multicasted.connect()
+
+setTimeout(() => {
+	subscription2 = multicasted.subscribe({
+		next: v => console.log('observerB: ' + v),
+	})
+}, 600)
+
+setTimeout(() => {
+	subscription1.unsubscribe()
+}, 1200)
+
+// 这里我们应该取消共享的 Observable 执行的订阅，
+// 因为此后 `multicasted` 将不再有订阅者
+setTimeout(() => {
+	subscription2.unsubscribe()
+	subscriptionConnect.unsubscribe() // 用于共享的 Observable 执行
+}, 2000)
